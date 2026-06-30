03d115238bd8d3037856b7871929f9fcc0aa74fc
Hofstetter Benjamin <benjamin.hofstetter@zazuko.com>
2026-06-30 08:48:53 +0200

    initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,21 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
